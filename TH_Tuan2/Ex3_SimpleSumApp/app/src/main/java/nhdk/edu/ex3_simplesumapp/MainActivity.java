84827bdb107197684b2e713d6444b9bfaa482909
Nguyen Khang <kayzociii@gmail.com>
2024-03-04 10:18:04 +0700

    Update Ex3_SimpleSumApp
    Update code xử lý backend

--- TH_Tuan2/Ex3_SimpleSumApp/app/src/main/java/nhdk/edu/ex3_simplesumapp/MainActivity.java
+++ TH_Tuan2/Ex3_SimpleSumApp/app/src/main/java/nhdk/edu/ex3_simplesumapp/MainActivity.java
@@ -3,6 +3,8 @@ package nhdk.edu.ex3_simplesumapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void XuLyCong(View view){
+        EditText editTextsoA = findViewById(R.id.edtA);
+        EditText editTextsoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKetQua);
+        String strA = editTextsoA.getText().toString();
+        String strB = editTextsoB.getText().toString();
+
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+
+        int Tong = so_A + so_B;
+        String strTong = String.valueOf(Tong);
+
+        editTextKetQua.setText(strTong);3
     }
 }
