86214fe0f09c3f945d6cea680ae298754fc99bf5
Nguyen Khang <kayzociii@gmail.com>
2024-03-24 17:19:37 +0700

    Update Cau2_AppBMI
    Xử lý main

--- Cau2_AppBMI/app/src/main/java/ntu/SV63134290/MainActivity.java
+++ Cau2_AppBMI/app/src/main/java/ntu/SV63134290/MainActivity.java
@@ -3,12 +3,36 @@ package ntu.SV63134290;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtChieuCao,edtCanNang;
+    Button btnTinh;
+
+    TextView txtKetQua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtChieuCao = findViewById(R.id.edtChieuCao);
+        edtCanNang = findViewById(R.id.edtCanNang);
+        btnTinh = findViewById(R.id.btnTinh);
+        txtKetQua = findViewById(R.id.txtKetQua);
+
+        btnTinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tinhBMI();
+            }
+        });
+    }
+
+    private void tinhBMI(){
+
     }
 }
