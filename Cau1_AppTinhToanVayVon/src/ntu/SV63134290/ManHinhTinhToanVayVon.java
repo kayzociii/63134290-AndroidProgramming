1b070dc17a76e5ce4e6b2bb770e7e228ed522568
Nguyen Khang <kayzociii@gmail.com>
2024-03-24 12:33:12 +0700

    Update Cau1_AppTinhToanVayVon
    Tạo JFrame

---
+++ Cau1_AppTinhToanVayVon/src/ntu/SV63134290/ManHinhTinhToanVayVon.java
@@ -0,0 +1,42 @@
+package ntu.SV63134290;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ManHinhTinhToanVayVon extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ManHinhTinhToanVayVon frame = new ManHinhTinhToanVayVon();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ManHinhTinhToanVayVon() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
